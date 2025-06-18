c3ed1b7db9dfcec8567445585a88e9940d5bb813
hemanthjangam <hemanthjangam.edu@gmail.com>
2025-06-18 23:22:03 +0530

    new code added

---
+++ LinkedList/SinglyLL/SearchInLL.java
@@ -0,0 +1,53 @@
+package LinkedList.SinglyLL;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data1, Node next1) {
+        this.data = data1;
+        this.next = next1;
+    }
+
+    Node(int data1) {
+        this.data = data1;
+        this.next = null;
+    }
+}
+
+public class SearchInLL {
+
+    private static boolean search(Node head, int target) {
+        Node cur = head;
+        if(cur == null) return false;
+        while(cur != null) {
+            if(cur.data == target) return true;
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    private static void printList(Node head) {
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data);
+            if (curr.next != null) System.out.print("->");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 4, 5, 6, 3};
+        Node n5 = new Node(nums[4]);
+        Node n4 = new Node(nums[3], n5);
+        Node n3 = new Node(nums[2], n4);
+        Node n2 = new Node(nums[1], n3);
+        Node n1 = new Node(nums[0], n2);
+        int target = 34;
+        Node head = n1;
+        printList(head);
+        boolean result = search(head, target);
+        System.out.println(result);
+    }
+}
