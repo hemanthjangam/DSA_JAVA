4a7b07a0309debc922b2f68df8d33d634e641047
hemanthjangam <hemanthjangam.edu@gmail.com>
2025-06-22 09:23:13 +0530

    new code added

---
+++ LinkedList/MediumSLL/DetectLoopInLL.java
@@ -0,0 +1,74 @@
+package LinkedList.MediumSLL;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// Problem statement:
+// detect a loop in a linked list
+
+// Example1:       
+// LL : 1 -> 2 -> 3 -> 4 -> 5
+//                    ^     |
+//                    |_____|
+// Output = true
+
+// Example2:
+// LL: 1 -> 2 -> 5 -> 6 -> 7 -> 8 -> 9
+//               ^                   |
+//               |___________________|                    
+// Output = true
+
+// Example2:
+// LL: 1 -> 2 -> 5 -> 6 ->                 
+// Output = false
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data1, Node next1) {
+        this.data = data1;
+        this.next = next1;
+    }
+
+    Node(int data1) {
+        this.data = data1;
+        this.next = null;
+    }
+}
+
+public class DetectLoopInLL {
+
+    private static Node createLl(int[] nums) {
+        Node head = new Node(nums[0]);
+        Node temp = head, copy = null;
+        for(int i = 1; i < nums.length; i++) {
+            Node newNode = new Node(nums[i]);
+            temp.next = newNode;
+            temp = newNode;
+            if(i == 2){
+                copy = temp;
+            }
+        }
+        temp.next = copy;
+        return head;
+    }
+
+    private static boolean detect(Node head) {
+        Map<Node, Integer> map = new HashMap<>();
+        while(head != null) {
+            if(map.containsKey(head)) return true;
+            map.put(head, 1);
+            head = head.next;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5};
+        Node head = createLl(nums);
+        System.out.println("1 -> 2 -> 3 -> 4 -> 5 -> 3");
+        boolean result = detect(head);
+        System.out.println(result);
+    }
+}
